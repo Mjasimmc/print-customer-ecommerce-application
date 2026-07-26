dd860ccbe671cbfba18ce19413af42cf90a532ed
mjasimmc <mjasimmc@gmail.com>
2026-07-26 21:27:32 +0530

    first commit

--- src/providers/AppProvider.tsx
+++ src/providers/AppProvider.tsx
@@ -1,6 +1,5 @@
 import React, { ReactNode } from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { AuthProvider } from '@print-delivery/auth';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -14,7 +13,7 @@ const queryClient = new QueryClient({
 export const AppProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
   return (
     <QueryClientProvider client={queryClient}>
-      <AuthProvider>{children}</AuthProvider>
+      {children}
     </QueryClientProvider>
   );
 };
